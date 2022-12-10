03bcc02241da487c4549256263ccd449d666742f
= <=>
2022-12-09 23:29:12 -0300

    Soluçao do exercicio FGTS

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Datas
+{
+  class Program
+    {
+      static void Main(string[] args)
+      {
+        decimal salario, fgts;
+
+        Console.Write("Salário (R$)..: ");
+        salario = Convert.ToDecimal(Console.ReadLine());
+
+        fgts = (salario / 100) * 8;
+
+        Console.WriteLine($"\nFGTS: {fgts:C2}");
+     }
+  }
+}
+
